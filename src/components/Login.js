9ba0e99c2f3ded7429bb74148b051bb63a861174
Ashutosh Chaubey <ashutoshchaubeymail@gmail.com>
2024-01-31 00:27:19 +0530

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,63 @@
+import React  from "react"
+import { useState } from "react"
+import axios from "axios"
+import './Login.css'
+import {useNavigate, Link} from "react-router-dom"
+
+
+
+function Login(){
+    const navigate=useNavigate()
+
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+
+    async function handleSubmit(e){
+        e.preventDefault()
+
+        try{
+            await axios.post("http://localhost:3000/login",{
+                email,password
+            })
+            .then(res=>{
+                if(res.data=="exist"){
+                    navigate("/home",{state:{id:email}})
+                }
+                else if(res.data=="notexist"){
+                    alert("User does not exist")
+                }
+                else{
+                    alert("wrong password")
+                }
+            })
+            .catch(e=>{
+                alert("wrong details")
+                console.log(e)  
+            })
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+
+    return (
+        <div className="login">
+            <h1>Login</h1>
+
+            <form action="POST">
+                <input type="email" onChange={(e) => { setEmail(e.target.value) }} placeholder="Email"  />
+                <input type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password"  />
+                <input type="submit" onClick={handleSubmit} />
+
+            </form>
+
+            <br />
+            <p>OR</p>
+            <br />
+
+            <Link to="/signup">New User</Link>
+
+        </div>
+    )
+}
+export default Login
---
+++ src/components/Signup.js
@@ -0,0 +1,67 @@
+import React, {useState } from "react"
+import axios from "axios"
+// import "./Signup.css"
+import { useNavigate, Link } from "react-router-dom"
+
+
+function Login() {
+    const navigate=useNavigate();
+
+    const [email,setEmail]=useState('')
+    const [mobile,setMobile]=useState('')
+    const [password,setPassword]=useState('')
+
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        try{
+
+            await axios.post("http://localhost:3000/signup",{
+                email,mobile,password
+            })
+            .then(res=>{
+                if(res.data=="exist"){
+                    alert("User already exists")
+                }
+                else if(res.data=="notexist"){
+                    navigate("/home",{state:{id:email}})
+                }
+            })
+            .catch(e=>{
+                alert("wrong details")
+                console.log(e);
+            })
+
+        }
+        catch(err){
+            console.log(err);
+
+        }
+
+    }
+
+
+    return (
+        <div className="login">
+
+            <h1>Signup</h1>
+
+            <form action="POST">
+                <input type="email" onChange={(e) => { setEmail(e.target.value) }} placeholder="Email"  />
+                <input type="mobile" onChange={(e) => { setMobile(e.target.value) }} placeholder="Mobile" />
+                <input type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password" />
+                <input type="submit" onClick={handleSubmit} />
+
+            </form>
+
+            <br />
+            <p>OR</p>
+            <br />
+
+            <Link to="/login">Login Page</Link>
+
+        </div>
+    )
+}
+
+export default Login
